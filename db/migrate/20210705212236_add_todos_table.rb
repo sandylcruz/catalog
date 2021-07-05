b412e10d014a1579099b8db7d23a9b17a2cd3074
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-05 14:29:01 -0700

    Migrate todos table

---
+++ db/migrate/20210705212236_add_todos_table.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# This is a migration to add the todos table
+class AddTodosTable < ActiveRecord::Migration[6.1]
+  def change
+    create_table :todos do |t|
+      t.string 'title', null: false
+      t.string 'body', null: false
+      t.boolean 'done', null: false
+    end
+
+    add_index :todos, :title
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_04_174103) do
+ActiveRecord::Schema.define(version: 2021_07_05_212236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "body", null: false
+    t.boolean "done", null: false
+    t.index ["title"], name: "index_todos_on_title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
