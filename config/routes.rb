ce45a500b7d806b89a25ad0707dca743c6c0cdd5
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-04 17:01:20 -0700

    Seed users database

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@
 
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    resource :users, only: %i[create show]
+    resources :users, only: %i[create show]
+    resource :session, only: %i[create destroy]
   end
   root 'static_pages#root'
 end
