96f761203db14f2531681e74024fe78bc1e579c0
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-05 14:21:16 -0700

    Finish users controller spec

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -14,20 +14,10 @@ module Api
       end
     end
 
-    def show
-      @user = User.find_by_id(params[:id])
-
-      if @user
-        render :show
-      else
-        render json: ['User does not exist'], status: 404
-      end
-    end
-
     private
 
     def user_params
-      params.permit(:username, :password_digest, :session_token)
+      params.permit(:username, :password, :password_digest, :session_token)
     end
   end
 end
--- spec/controllers/api/users_controller_spec.rb
+++ spec/controllers/api/users_controller_spec.rb
@@ -5,22 +5,25 @@ require 'rails_helper'
 RSpec.describe Api::UsersController, type: :controller do
   describe 'POST #create' do
     it 'should create a new user if valid credentials are supplied' do
-      # post :create,
-      # params: { user: { username: 'cats' , password: 'password' }},
-      # format: :json
-      # expect(response).to have_http_status(200)
-      # # expect(response).to render_template(:show)
+      post :create,
+           params: { username: 'cats', password: 'password' },
+           format: :json
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:show)
     end
 
     it 'should not create a new user if invalid credentials are supplied' do
+      post :create,
+           params: { username: '', password: '' },
+           format: :json
+      expect(response).to have_http_status(422)
     end
 
     it 'should validate password length is at least 6 characters' do
-    end
-  end
-
-  describe 'GET #show' do
-    it 'should return user if given valid user id' do
+      post :create,
+           params: { username: 'cats123', password: '12' },
+           format: :json
+      expect(response).to have_http_status(422)
     end
   end
 end
