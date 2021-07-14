7ecbd20c91998f8275b7ab687275f577a76f273b
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-14 11:21:20 -0700

    Update sidebar

--- app/javascript/src/Sidebar/Sidebar.js
+++ app/javascript/src/Sidebar/Sidebar.js
@@ -2,6 +2,27 @@ import React from 'react';
 
 import styled from 'styled-components';
 
+const A = styled.a`
+   font-size: 20px;
+    text-transform: uppercase;
+    padding: 2rem 0;
+    font-weight: bold;
+    letter-spacing: 0.5rem;
+    color: #0D0C1D;
+    text-decoration: none;
+    transition: color 0.3s linear;
+
+    @media (max-width: 576px) {
+      font-size: 1.5rem;
+      text-align: center;
+    }
+
+    &:hover {
+      color: #343078;
+    }
+  }
+`;
+
 const H3 = styled.h3`
   font-size; 900px;
   margin: 20px;
@@ -10,18 +31,36 @@ const H3 = styled.h3`
 
 const SidebarContainer = styled.div`
   background-color: #f5f7f7;
-  // background-color: #DFE8E7;
-  width: 180px;
+  width: 220px;
   height: 100vh;
 `;
 
 const Sidebar = () => {
   return (
     <SidebarContainer>
-      <H3>Home</H3>
-      <H3>Lists</H3>
-      <H3>Tags</H3>
-      <H3>Filters</H3>
+      <H3>
+        <A href="/" aria-label="home">
+          Home
+        </A>
+      </H3>
+      <H3>
+        {' '}
+        <A href="/" aria-label="lists">
+          Lists
+        </A>
+      </H3>
+      <H3>
+        {' '}
+        <A href="/" aria-label="tags">
+          Tags
+        </A>
+      </H3>
+      <H3>
+        {' '}
+        <A href="/" aria-label="filters">
+          Filters
+        </A>
+      </H3>
     </SidebarContainer>
   );
 };
