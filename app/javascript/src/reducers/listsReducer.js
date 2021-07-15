3911e409e1d4474484473ce6cc6b3d15641c90fa
Sandy Cruz <sandyleahcruz@icloud.com>
2021-07-14 18:51:16 -0700

    Write lists reducer

--- app/javascript/src/reducers/listsReducer.js
+++ app/javascript/src/reducers/listsReducer.js
@@ -1,7 +1,42 @@
-const listsReducer = (state = {}, action) => {
+import {
+  RECEIVE_LIST,
+  RECEIVE_LISTS,
+  REMOVE_LIST,
+  UPDATE_LIST,
+} from '../actions/listActions';
+
+const defaultState = {};
+
+const listsReducer = (state = defaultState, action) => {
+  Object.freeze(state);
+
   switch (action.type) {
-    default:
+    case 'RECEIVE_LIST': {
+      const nextState = { ...state };
+      nextState[action.list.id] = action.list;
+      return nextState;
+    }
+    case 'RECEIVE_LISTS': {
+      const nextState = { ...state };
+
+      actions.lists.forEach((list) => {
+        nextState[list.id] = list;
+      });
+
+      return nextState;
+    }
+    case 'REMOVE_LIST': {
+      const nextState = { ...state };
+      delete nextState[action.list.id];
+      return nextState;
+    }
+    case 'UPDATE_LIST': {
+      const nextState = { ...state, [action.list.id]: action.list };
+      return nextState;
+    }
+    default: {
       return state;
+    }
   }
 };
 
